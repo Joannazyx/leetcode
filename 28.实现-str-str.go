18f89e0cbac90c746bb0e9342199abb4cce79f03
Zhang Yuxuan <joannazyx@gmail.com>
2021-07-26 15:19:45 +0800

    lt28
    字符串检索。
    go strings里有库函数。

---
+++ 28.实现-str-str.go
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=golang
+ *
+ * [28] 实现 strStr()
+ */
+
+// @lc code=start
+func strStr(haystack string, needle string) int {
+	return strings.Index(haystack, needle)
+
+}
+
+// @lc code=end
+
